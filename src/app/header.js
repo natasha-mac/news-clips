49e9d78af5bc1a651ba38f356f282dfbd9acc705
Natasha Machado <natasha.machado2000@gmail.com>
2024-04-28 14:36:51 -0400

    UI updates

--- src/app/header.js
+++ src/app/header.js
@@ -1,36 +1,21 @@
-import { AppBar, Toolbar, styled, IconButton } from "@mui/material";
-import { Menu, Brightness4, Brightness7 } from "@mui/icons-material";
+import { AppBar, styled } from "@mui/material";
 import Image from "next/image";
 
 const StyledApp = styled(AppBar)`
-  background: #fff; /* Default background color */
+  background: #fff;
   height: 70px;
-`;
-
-const MenuIcon = styled(Menu)`
-  color: #000; /* Default color */
 `;
 
 const Logo = styled("div")({
   margin: "auto",
-  paddingRight: 70,
 });
-
-const ThemeToggle = styled(IconButton)`
-  position: absolute;
-  top: 12px;
-  right: 12px;
-`;
 
 const Header = () => {
   return (
     <StyledApp>
-      <Toolbar>
-        <MenuIcon />
-        <Logo>
-          <Image src="/Logo.png" width={70} height={70} alt="Logo" />
-        </Logo>
-      </Toolbar>
+      <Logo>
+        <Image src="/Logo.png" width={70} height={70} alt="Logo" />
+      </Logo>
     </StyledApp>
   );
 };
